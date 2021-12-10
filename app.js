7554b919ec54bbabc7b44182d2412c25f1a2f7cf
Malvin <malvinmusanhi3@gmail.com>
2021-12-10 06:26:14 -0500

    Add final touches

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const gameChoices = ['rock', 'paper', 'scissors']
 let computerSelection;
 let playerSelection;
+let playerScore = 0;
+let computerScore= 0;
 
 function computerPlay() {
   return gameChoices[Math.floor(Math.random() * gameChoices.length)];
@@ -14,15 +16,54 @@ function playRound(playerSelection, computerSelection) {
   computerSelection = computerPlay();
   playerSelection = userPlay();
   
+
+  // Determines whether player wins
   if (playerSelection === computerSelection) {
     console.log("It is a tie!")
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     console.log("Rock crushes scissors. User Wins!")
+    playerScore++;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     console.log("Paper covers rock. User wins!");
+    playerScore++;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     console.log("Scissors cuts paper. User wins!");
+    playerScore++;
+
+    // Determins whether the computer wins
+  } else if (computerSelection === "rock" && userChoice === "scissors") {
+    console.log("Rock crushes scissors. Computer wins!");
+    computerScore++;
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    console.log("Paper covers rock. Computer wins!");
+    computerScore++;
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    console.log("Scissors cuts paper. Computer wins!");
+    computerScore++;
   }
 }
 
-playRound();
+function showScoreSheet() {
+  console.log("Score:");
+  console.log("User: " + playerScore);
+  console.log("Computer: " + computerScore);
+
+  if (playerScore === computerScore) {
+    console.log("Game over. It is a tie!");
+  } else if (playerScore > computerScore) {
+    console.log("Game over. User wins!");
+  } else {
+    console.log("Game over. Computer wins!");
+  }
+}
+
+function game() {
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  showScoreSheet();
+}
+
+game();
